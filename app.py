edab85e540f39cfe8e2766615b0a42e2cff0559a
wellington90 <126090580+wellington90@users.noreply.github.com>
2023-06-30 12:59:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Olá, mundo!2023-2'
+    return 'Olá, mundo!30/06/2023'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
